b3c90257020addd67a221abe5283f1e025364058
Sam Davies <sam@razeware.com>
2017-07-12 08:10:31 +0700

    Initial Commit

---
+++ GuardpostKit/GuardpostKit.h
@@ -0,0 +1,19 @@
+//
+//  GuardpostKit.h
+//  GuardpostKit
+//
+//  Created by Sam Davies on 12/07/2017.
+//  Copyright © 2017 Razeware. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for GuardpostKit.
+FOUNDATION_EXPORT double GuardpostKitVersionNumber;
+
+//! Project version string for GuardpostKit.
+FOUNDATION_EXPORT const unsigned char GuardpostKitVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <GuardpostKit/PublicHeader.h>
+
+
